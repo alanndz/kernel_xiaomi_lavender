64386ef0e6f843c50d6030990b4715b6e87745aa
Mark Salyzyn <salyzyn@google.com>
2019-02-21 12:45:59 -0800

    ANDROID: overlayfs: override_creds=off option bypass creator_cred
    By default, all access to the upper, lower and work directories is the
    recorded mounter's MAC and DAC credentials.  The incoming accesses are
    checked against the caller's credentials.
    
    If the principles of least privilege are applied, the mounter's
    credentials might not overlap the credentials of the caller's when
    accessing the overlayfs filesystem.  For example, a file that a lower
    DAC privileged caller can execute, is MAC denied to the generally
    higher DAC privileged mounter, to prevent an attack vector.
    
    We add the option to turn off override_creds in the mount options; all
    subsequent operations after mount on the filesystem will be only the
    caller's credentials.  The module boolean parameter and mount option
    override_creds is also added as a presence check for this "feature",
    existence of /sys/module/overlay/parameters/override_creds.
    
    It was not always this way.  Circa 4.6 there was no recorded mounter's
    credentials, instead privileged access to upper or work directories
    were temporarily increased to perform the operations.  The MAC
    (selinux) policies were caller's in all cases.  override_creds=off
    partially returns us to this older access model minus the insecure
    temporary credential increases.  This is to permit use in a system
    with non-overlapping security models for each executable including
    the agent that mounts the overlayfs filesystem.  In Android
    this is the case since init, which performs the mount operations,
    has a minimal MAC set of privileges to reduce any attack surface,
    and services that use the content have a different set of MAC
    privileges (eg: read, for vendor labelled configuration, execute for
    vendor libraries and modules).  The caveats are not a problem in
    the Android usage model, however they should be fixed for
    completeness and for general use in time.
    
    Signed-off-by: Mark Salyzyn <salyzyn@android.com>
    Cc: Miklos Szeredi <miklos@szeredi.hu>
    Cc: Jonathan Corbet <corbet@lwn.net>
    Cc: Vivek Goyal <vgoyal@redhat.com>
    Cc: Eric W. Biederman <ebiederm@xmission.com>
    Cc: Amir Goldstein <amir73il@gmail.com>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Cc: Stephen Smalley <sds@tycho.nsa.gov>
    Cc: linux-unionfs@vger.kernel.org
    Cc: linux-doc@vger.kernel.org
    Cc: linux-kernel@vger.kernel.org
    Cc: kernel-team@android.com
    ---
    ---
    v9:
    - Add to the caveats
    
    v8:
    - drop pr_warn message after straw poll to remove it.
    - added a use case in the commit message
    
    v7:
    - change name of internal parameter to ovl_override_creds_def
    - report override_creds only if different than default
    
    v6:
    - Drop CONFIG_OVERLAY_FS_OVERRIDE_CREDS.
    - Do better with the documentation.
    - pr_warn message adjusted to report consequences.
    
    v5:
    - beefed up the caveats in the Documentation
    - Is dependent on
      "overlayfs: check CAP_DAC_READ_SEARCH before issuing exportfs_decode_fh"
      "overlayfs: check CAP_MKNOD before issuing vfs_whiteout"
    - Added prwarn when override_creds=off
    
    v4:
    - spelling and grammar errors in text
    
    v3:
    - Change name from caller_credentials / creator_credentials to the
      boolean override_creds.
    - Changed from creator to mounter credentials.
    - Updated and fortified the documentation.
    - Added CONFIG_OVERLAY_FS_OVERRIDE_CREDS
    
    v2:
    - Forward port changed attr to stat, resulting in a build error.
    - altered commit message.
    
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    (cherry picked from https://lore.kernel.org/patchwork/patch/1009299)
    Bug: 109821005
    Bug: 112955896
    Bug: 126256072
    Bug: 127298877
    Change-Id: Ie43b0c7dfd64f4cfc27dfe5e1622ea01f3b000cf

--- fs/overlayfs/dir.c
+++ fs/overlayfs/dir.c
@@ -420,10 +420,7 @@ static int ovl_create_or_link(struct dentry *dentry, int mode, dev_t rdev,
 			put_cred(override_creds(override_cred));
 			put_cred(override_cred);
 
-			err = ovl_create_over_whiteout(dentry, inode, &stat,
-						       link, hardlink);
-		}
-		revert_creds(old_cred);
+		ovl_revert_creds(old_cred);
 	}
 
 	if (!err)
@@ -657,7 +654,7 @@ static int ovl_do_remove(struct dentry *dentry, bool is_dir)
 
 		err = ovl_remove_and_whiteout(dentry, is_dir);
 
-		revert_creds(old_cred);
+		ovl_revert_creds(old_cred);
 	}
 out_drop_write:
 	ovl_drop_write(dentry);
@@ -896,8 +893,7 @@ out_dput_old:
 out_unlock:
 	unlock_rename(new_upperdir, old_upperdir);
 out_revert_creds:
-	if (old_opaque || new_opaque)
-		revert_creds(old_cred);
+	ovl_revert_creds(old_cred);
 out_drop_write:
 	ovl_drop_write(old);
 out:
