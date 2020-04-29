8481c965ad9b9e54663946fa9ac53b7f3654d222
alanndz <alanmahmud0@gmail.com>
2020-04-29 18:48:37 +0800

    kernel: futex: fix
    Signed-off-by: alanndz <alanmahmud0@gmail.com>

--- kernel/futex.c
+++ kernel/futex.c
@@ -404,7 +404,7 @@ static void get_futex_key_refs(union futex_key *key)
 
 	switch (key->both.offset & (FUT_OFF_INODE|FUT_OFF_MMSHARED)) {
 	case FUT_OFF_INODE:
-		ihold(key->shared.inode); /* implies smp_mb(); (B) */
+		ihold(key->shared.i_seq); /* implies smp_mb(); (B) */
 		break;
 	case FUT_OFF_MMSHARED:
 		futex_get_mm(key); /* implies smp_mb(); (B) */
